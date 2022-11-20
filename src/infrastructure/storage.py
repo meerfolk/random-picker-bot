7818d18a4dc586aabe7f6235f9203499bd0c5fa7
Vadim Osokin <Vadim.Osokin@leroymerlin.ru>
2022-11-20 22:15:55 +0600

    Use lazy redis connection

--- src/infrastructure/storage.py
+++ src/infrastructure/storage.py
@@ -2,10 +2,19 @@
 import redis
 import os
 
-REDIS_HOST = os.environ['REDIS_HOST']
-REDIS_PREFIX = 'picking_daily_anekdot:'
+REDIS_HOST=""
+REDIS_PREFIX=""
+r = None
 
-r = redis.Redis(host=REDIS_HOST, port=6379, db=0)
+def __tryInitRedis():
+    global REDIS_HOST
+    global REDIS_PREFIX
+    global r
+
+    REDIS_HOST = os.environ['REDIS_HOST']
+    REDIS_PREFIX = 'picking_daily_anekdot:'
+
+    r = redis.Redis(host=REDIS_HOST, port=6379, db=0)
 
 def __getFullJokersSetKey():
     return f'{REDIS_PREFIX}jokers:list:full'
@@ -25,12 +34,15 @@ def __restoreJokersSet():
     r.copy(__getFullJokersSetKey(), __getNextJokersSetKey(), replace=True)
 
 def getFullList():
+    __tryInitRedis()
     return __getSet(__getFullJokersSetKey())
 
 def getNetxtJokersList():
+    __tryInitRedis()
     return __getSet(__getNextJokersSetKey())
 
 def getNextJoker():
+    __tryInitRedis()
     nextJoker: bytes = r.spop(__getNextJokersSetKey())
 
     if __getSetSize(__getNextJokersSetKey()) == 0:
@@ -39,5 +51,6 @@ def getNextJoker():
     return nextJoker.decode()
 
 def addJoker(joker):
+    __tryInitRedis()
     r.sadd(__getFullJokersSetKey(), joker)
     r.sadd(__getNextJokersSetKey(), joker)
